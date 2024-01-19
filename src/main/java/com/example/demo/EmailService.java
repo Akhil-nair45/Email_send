52ee01cab1527c1e1c39a89db2d4ef85bb8df38c
Akhil Nair <Akhil.Nair@einfochips.com>
2024-01-19 13:33:45 +0530

    Email Spring

---
+++ src/main/java/com/example/demo/EmailService.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import java.io.File;
+import java.util.Objects;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+	
+	private final JavaMailSender javaMailSender;
+
+	public EmailService(JavaMailSender javaMailSender) {
+		super();
+		this.javaMailSender = javaMailSender;
+	}
+	
+	
+	@PostConstruct
+	public void sendEmailWithAttachment() throws MessagingException
+	{
+		MimeMessage createMimeMessage = javaMailSender.createMimeMessage();
+		
+		MimeMessageHelper mm= new MimeMessageHelper(createMimeMessage,true);//this will throw exception
+		
+		mm.setFrom("nairakhilrs45@gmail.com");
+		mm.setTo("nairakhilashok@gmail.com");
+		mm.setText("Hey this is a test email as i have implemented the email from spring boot, Thanks for ur understanding and support!");
+		mm.setSubject("This is a dummy email");
+		
+		FileSystemResource fr= new FileSystemResource(new File("C:\\Users\\158250\\EmailSpring\\capture.png"));
+		mm.addAttachment(Objects.requireNonNull(fr.getFilename()),fr);
+		
+		javaMailSender.send(createMimeMessage);
+		System.out.println("Mail send to user successfully!!!!");
+	}
+}
